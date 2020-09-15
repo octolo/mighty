a14f7427f7234e8ba3aefb8c1d988381cf7ec5dc
hicinformatic <hicinformatic@gmail.com>
2020-09-15 19:06:19 +0000

    actions config translate

---
+++ management/commands/addConfigClient.py
@@ -0,0 +1,24 @@
+from django.core.management.base import CommandError
+from mighty.management import ModelBaseCommand
+from mighty.models import ConfigClient
+import os.path, json
+
+class Command(ModelBaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--name')
+        parser.add_argument('--json')
+        super().add_arguments(parser)
+
+    def handle(self, *args, **options):
+        self.name = options.get('name')
+        self.json = options.get('json')
+        if not os.path.isfile(self.json):
+            raise CommandError('JSON "%s" does not exist' % self.json)
+        super().handle(*args, **options)
+
+    def do(self):
+        with open(self.json, encoding=self.encoding) as json_file:
+            conf, status = ConfigClient.objects.get_or_create(name=self.name)
+            conf.config = json.load(json_file)
+            conf.save()
+
