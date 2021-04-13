302f00840f8e26f0baa0cd741d55151ec2f90156
hicinformatic <hicinformatic@gmail.com>
2021-04-13 02:03:43 +0200

    improve pdf

--- views.py
+++ views.py
@@ -313,6 +313,7 @@ class GenericSuccess(View):
     def get(self, request):
         return HttpResponse('OK')
 
+import os
 class PDFView(DetailView):
     header_html = None
     footer_html = None
@@ -320,36 +321,36 @@ class PDFView(DetailView):
     in_browser = False
     pdf_name = 'file.pdf'
     options = conf.pdf_options
-    header_footer = conf.pdf_header_footer
+    header_tpl = conf.pdf_header
+    footer_tpl = conf.pdf_footer
     content_html = conf.pdf_content
+    tmp_pdf = None
 
     def get_object(self):
         if not self.cache_object:
             self.cache_object = super().get_object()
         return self.cache_object
 
-    def build_footer_html(self):
-        with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as footer_html:
-            footer = self.header_footer % self.get_object().group.build_document_footer
-
     def get_header(self):
         return ""
 
     def build_header_html(self):
-        with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as header_html:
-            header = self.get_header()
-            header_html.write(Template(header).render(self.get_context_data()).encode("utf-8"))
-        self.header_html = header_html
+        if not self.header_html:
+            with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as header_html:
+                header = self.get_header()
+                header_html.write(Template(header).render(self.get_context_data()).encode("utf-8"))
+            self.header_html = header_html
         return self.header_html
 
     def get_footer(self):
         return ""
 
     def build_footer_html(self):
-        with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as footer_html:
-            footer = self.get_footer()
-            footer_html.write(Template(footer).render(self.get_context_data()).encode("utf-8"))
-        self.footer_html = footer_html
+        if not self.footer_html:
+            with tempfile.NamedTemporaryFile(suffix='.html', delete=False) as footer_html:
+                footer = self.get_footer()
+                footer_html.write(Template(footer).render(self.get_context_data()).encode("utf-8"))
+            self.footer_html = footer_html
         return self.footer_html
 
     def get_css_print(self):
@@ -368,27 +369,40 @@ class PDFView(DetailView):
     def get_pdf_name(self):
         return self.pdf_name
 
-    def tmp_pdf(self, context):
-        with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as tmp_pdf:
-            return pdfkit.from_string(self.get_template(context), tmp_pdf.name, options=self.get_options())
+    def get_tmp_pdf(self, context):
+        if not self.tmp_pdf:
+            with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as tmp_pdf:
+                pdfkit.from_string(self.get_template(context), tmp_pdf.name, options=self.get_options())
+                self.tmp_pdf = tmp_pdf
+        return self.tmp_pdf
 
     def save_pdf(self, context):
-        tmp_pdf = self.tmp_pdf(context)
+        tmp_pdf = self.get_tmp_pdf(context)
 
     def get_template(self, context):
         template_name = self.get_template_names()
         template = get_template(template_name)
         return self.content_html % template.render(context)
 
+    def clean_tmp(self):
+        if self.header_html and os.path.isfile(self.header_html.name):
+            os.remove(self.header_html.name)
+        if self.footer_html and os.path.isfile(self.footer_html.name):
+            os.remove(self.footer_html.name)
+        if self.tmp_pdf and os.path.isfile(self.tmp_pdf.name):
+            os.remove(self.tmp_pdf.name)
+
     def render_to_response(self, context, **response_kwargs):
         if self.request.GET.get('save', False): self.save_pdf(context)
         if self.in_browser:
-            with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as tmp_pdf:
+            with tempfile.NamedTemporaryFile(suffix='.pdf', delete=True) as tmp_pdf:
                 pdf = pdfkit.from_string(self.get_template(context), tmp_pdf.name, options=self.get_options())
+                self.clean_tmp()
                 return FileResponse(open(tmp_pdf.name, 'rb'), filename=self.get_pdf_name())
         pdf = pdfkit.from_string(self.get_template(context), False, options=self.get_options())
         response = HttpResponse(pdf, content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="%s"' % self.get_pdf_name()
+        self.clean_tmp()
         return response
 
 if 'rest_framework' in setting('INSTALLED_APPS'):
