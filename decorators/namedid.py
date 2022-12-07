aa681caa915c021b2c53270fd88b9149a235d173
hicinformatic <hicinformatic@gmail.com>
2022-12-07 17:41:33 +0100

    improve namedid

--- decorators/namedid.py
+++ decorators/namedid.py
@@ -10,10 +10,18 @@ def NamedIdModel(**kwargs):
             class Meta(obj.Meta):
                 abstract = True
 
+            @property
+            def Qasfield(self):
+                return {field: getattr(self, field) for field in self.named_id_fields}
+
+            @property
+            def count_named_id(self):
+                return self.qs_not_self.filter(**self.Qasfield).count()
+
             def set_named_id(self):
-                base_name = "-".join([getattr(self, field) for field in self.named_id_fields])
+                base_name = "-".join([str(getattr(self, field)) for field in self.named_id_fields])
                 named_id = slugify(base_name)
-                count = self.qs_not_self.count()
+                count = self.count_named_id
                 self.named_id = "%s-%s" % (named_id, count+1) if count > 0 else named_id
 
         return NamedIdModel
