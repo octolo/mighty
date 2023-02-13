f1e7021b9f3b5e3ce835140e092173e7c81278c2
acuD1 <arsciand@student.42.fr>
2023-02-13 16:16:02 +0100

    Fix attachments backend for sendinblue

--- applications/messenger/backends/email/sendinblue.py
+++ applications/messenger/backends/email/sendinblue.py
@@ -1,10 +1,11 @@
 from mighty.applications.messenger.backends import MissiveBackend
 from mighty.functions import setting
-from mighty.apps import MightyConfig
-from mighty.applications.messenger import choices
 from django.core.mail.message import make_msgid
-import requests, json, base64, sib_api_v3_sdk
+import os
+import base64
+import sib_api_v3_sdk
 from sib_api_v3_sdk.rest import ApiException
+
 
 class MissiveBackend(MissiveBackend):
     APIKEY = setting('SENDINBLUE_KEY', False)
@@ -16,9 +17,9 @@ class MissiveBackend(MissiveBackend):
     def api_instance(self):
         if not self.api_instance_cache:
             configuration = sib_api_v3_sdk.Configuration()
-            print(self.APIKEY)
             configuration.api_key['api-key'] = self.APIKEY
-            self.api_instance_cache = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+            self.api_instance_cache = sib_api_v3_sdk.TransactionalEmailsApi(
+                sib_api_v3_sdk.ApiClient(configuration))
         return self.api_instance_cache
 
     def email_attachments(self):
@@ -27,8 +28,8 @@ class MissiveBackend(MissiveBackend):
             logs = []
             for document in self.missive.attachments:
                 if setting('MISSIVE_SERVICE', False):
-                    attachements.append({
-                        "content": base64.b64encode(document.read()),
+                    attachments.append({
+                        "content": base64.b64encode(document.read()).decode('utf-8'),
                         "name": os.path.basename(document.name),
                     })
                 logs.append(os.path.basename(document.name))
@@ -39,23 +40,27 @@ class MissiveBackend(MissiveBackend):
         over_target = setting('MISSIVE_EMAIL', False)
         self.missive.target = over_target if over_target else self.missive.target
         self.logger.info("Email - from : %s, to : %s, reply : %s" %
-            (self.sender_email, self.missive.target, self.reply_email))
+                         (self.sender_email, self.missive.target, self.reply_email))
         if True:
             self.missive.msg_id = make_msgid()
             data = {
                 "to": [{"email": self.missive.target}],
                 "reply_to": {"email": self.reply_email, "name": self.reply_name},
-                "headers": { "charset":"iso-8859-1"},
+                "headers": {"charset": "iso-8859-1"},
                 "sender": {"email": self.missive.sender, "name": self.missive.name},
                 "subject": self.missive.subject,
             }
-            if self.missive.html_format: data["html_content"] = self.missive.html_format
-            else: data["text_content"] = str(self.missive.txt)
-            attachs = self.email_attachments()
-            if len(attachs): data["attachment"] = attachs
+            if self.missive.html_format:
+                data["html_content"] = self.missive.html_format
+            else:
+                data["text_content"] = str(self.missive.txt)
+            attachments = self.email_attachments()
+            if len(attachments):
+                data["attachment"] = attachments
             send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(**data)
             try:
-                api_response = self.api_instance.send_transac_email(send_smtp_email)
+                api_response = self.api_instance.send_transac_email(
+                    send_smtp_email)
                 print(api_response)
             except ApiException as e:
                 print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
