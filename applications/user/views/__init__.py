2b59b80e8832ab66ac1f7bc113c2077168ed379b
hicinformatic <hicinformatic@gmail.com>
2023-03-08 23:20:24 +0100

    clean views

--- applications/user/views/__init__.py
+++ applications/user/views/__init__.py
@@ -3,10 +3,3 @@ from mighty.applications.user.views.create import CreateUserView
 from mighty.applications.user.views.invitation import InvitationDetailView
 from mighty.applications.user.views.check import UserEmailCheckView, UserPhoneCheckView
 from mighty.applications.user.views.forms import CreatUserFormView
-
-__all__ = (
-    ProfileView,
-    CreateUserView,
-    InvitationDetailView,
-    UserEmailCheckView, UserPhoneCheckView,
-)
