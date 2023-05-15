3c91503d410b8dcd179a6af258a4a743e0e1e168
Pous17 <116793451+Pous17@users.noreply.github.com>
2023-05-15 15:19:10 +0200

    code: variable decorator

--- decorators/variable.py
+++ decorators/variable.py
@@ -1,14 +1,17 @@
 def EnableVariableEditorModel(**kwargs):
-    def wrapper(_class):
-        class Prop(_class):
+    def wrapper(obj):
+        class Prop(obj):    
+            class Meta(obj.Meta):
+                abstract = True
+
             def c_list(self) -> list[str]:
                 prop_list = [p for p in dir(self)]
                 _list = []
-                
+
                 for i in prop_list:
                     if kwargs.get("to_match") in i:
                         _list.append(i)
-                
+
                 return _list
             
             def v_list(self) -> list[str]:
@@ -18,23 +21,13 @@ def EnableVariableEditorModel(**kwargs):
                 if kwargs.get("lookup_list") == True:
                     for f in f_list:
                         try:
-                            _var = getattr(getattr(self, f), "var")
+                            _list.append({
+                                "var": f, 
+                                "desc": getattr(getattr(self, f), "desc"),
+                                "editor": getattr(getattr(self, f), "editor"),
+                            })
                         except:
-                            _var = None
-                        try:
-                            _desc = getattr(getattr(self, f), "desc")
-                        except:
-                            _desc = None
-                        try:
-                            _editor = getattr(getattr(self, f), "editor")
-                        except:
-                            _editor = None
-
-                        _list.append({
-                            "var": _var, 
-                            "desc": _desc,
-                            "editor": _editor,
-                        })
+                            pass
 
                 return _list
         return Prop
