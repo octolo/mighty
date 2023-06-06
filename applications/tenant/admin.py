f58ed6ec77bac7e5e12957c9fd2d25ecbccc1874
hicinformatic <hicinformatic@gmail.com>
2023-06-06 09:03:09 +0000

    fix three first

--- applications/tenant/admin.py
+++ applications/tenant/admin.py
@@ -8,7 +8,7 @@ class RoleAdmin(BaseAdmin):
     search_fields = ('name', 'group__search')
     list_display = ('name', 'is_immutable', 'group')
     fieldsets = ((None, {'classes': ('wide',), 'fields': fields.role}),)
-    readonly_fields = ('number',)
+    readonly_fields = ('number', 'three_first')
 
 class TenantAdmin(BaseAdmin):
     raw_id_fields = ('group', 'user')
