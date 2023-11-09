7556520886fcdaf1c91aaa809e6447380c443cce
hicinformatic <hicinformatic@gmail.com>
2023-11-09 20:40:04 +0000

    prepare scp

--- decorators/inherit.py
+++ decorators/inherit.py
@@ -1,22 +1,21 @@
 from django.utils.module_loading import import_string
 
-def InheritClassList(inherit_list=()):
+class InheritClass: pass
+
+def InheritClassList(*args):
     def decorator(cls):
-        inherit_classes = tuple(import_string(i) for i in inherit_list)
-        InheritClasses = type("InheritClasses", inherit_classes, {})
-        class NewClass(cls, InheritClasses): pass
+        inherit_classes = tuple(import_string(i) for i in args)
+        class NewClass(cls, type("InheritClass", inherit_classes, {})): pass
         NewClass.__name__ = cls.__name__
         return NewClass
     return decorator
 
-
-def InheritModelClassList(inherit_list=()):
+def InheritModelClassList(*args, **kwargs):
     def decorator(cls):
-        inherit_classes = tuple(import_string(i) for i in inherit_list)
-        InheritClasses = type("InheritClasses", inherit_classes, {})
-        class NewClass(cls, InheritClasses):
+        inherit_classes = tuple(import_string(i) for i in args)
+        class NewClass(cls, type("InheritClass", inherit_classes, {})):
             class Meta(cls.Meta):
-                abstract = True
+                abstract = kwargs.get("abstract", True)
         NewClass.__name__ = cls.__name__
         return NewClass
     return decorator
