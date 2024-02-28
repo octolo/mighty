533cff8b926266fcad97421d0c4f49fb73d2a857
hicinformatic <hicinformatic@gmail.com>
2024-02-28 16:32:47 +0100

    fix attr recurs with default

--- functions/facilities.py
+++ functions/facilities.py
@@ -1,4 +1,7 @@
 from functools import reduce
 
-def getattr_recursive(obj, strtoget, split="."):
-    return reduce(getattr, [obj, ]+strtoget.split(split)) if strtoget else strtoget
+def getattr_recursive(obj, strtoget, split=".", default=None):
+    try:
+        return reduce(getattr, [obj, ]+strtoget.split(split)) if strtoget else strtoget
+    except AttributeError:
+        return default
