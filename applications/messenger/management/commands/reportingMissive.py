35ee48c09b10b735b0eded844efccc45f9a58565
hicinformatic <hicinformatic@gmail.com>
2024-07-22 15:43:31 +0200

    add export missive

---
+++ applications/messenger/management/commands/reportingMissive.py
@@ -0,0 +1,61 @@
+from mighty.management import ModelBaseCommand
+from mighty.models import Missive
+import csv, datetime
+
+class Command(ModelBaseCommand):
+    model = Missive
+    fields = [
+        'uid',
+        'mode',
+        'subject',
+        'status',
+        'priority',
+        'name',
+        'sender',
+        'reply',
+        'reply_name',
+        'target',
+        'service',
+        'denomination',
+        'last_name',
+        'first_name',
+        'addr_backend_id',
+        'address',
+        'complement',
+        'locality',
+        'postal_code',
+        'state',
+        'state_code',
+        'country',
+        'country_code',
+        'cedex',
+        'cedex_code',
+        'special',
+        'index',
+        'raw',
+        'msg_id',
+        'partner_id',
+        'code_error',
+        'content_type',
+        'object_id',
+    ]
+
+    def before_job(self):
+        filename = 'reporting_missive_%s.csv' % datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+        self.csvfile = open(filename, 'w', newline='')
+        self.writer = csv.writer(self.csvfile)
+        self.writer.writerow(self.fields+['pages',])
+
+    def on_object(self, obj):
+        data = [getattr(obj, f) for f in self.fields]
+        if obj.mode in ("POSTAL", "POSTALAR"):
+            try:
+                documents = obj.check_documents()
+                documents = documents.get("documents")
+                pages = sum(doc.get("pages_count") for doc in documents)
+                data.append(pages)
+            except:
+                data.append("error")
+        else:
+            data.append(None)
+        self.writer.writerow(data)
