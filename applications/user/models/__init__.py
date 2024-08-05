4a4824fdeeca003f20dff001b098a5fc303b8beb
hicinformatic <hicinformatic@gmail.com>
2024-08-05 11:24:00 +0200

    remove changelog by model

--- applications/user/models/__init__.py
+++ applications/user/models/__init__.py
@@ -3,4 +3,4 @@ from mighty.applications.user.models.address import UserAddress
 from mighty.applications.user.models.phone import UserPhone
 from mighty.applications.user.models.mergeableaccount import MergeableAccount
 from mighty.applications.user.models.trashmail import Trashmail
-from mighty.applications.user.models.meta import InternetProtocol, UserAgent, UserAccessLogModel, UserChangeLogModel
+from mighty.applications.user.models.meta import InternetProtocol, UserAgent, UserAccessLogModel
