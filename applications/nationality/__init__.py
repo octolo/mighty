82e12783babb023e20f017e6ca0852a4497f56a8
acuD1 <arsciand@student.42.fr>
2024-09-11 16:48:09 +0200

    code: remove flag in __init__.py

--- applications/nationality/__init__.py
+++ applications/nationality/__init__.py
@@ -4,6 +4,5 @@ import json
 def conf_prefix_numbering():
     from mighty.models import Nationality
     return [
-        {"country": nat.country, "alpha2": nat.alpha2, "numbering": nat.numbering, "flag": nat.image_url}
+        {"country": nat.country, "alpha2": nat.alpha2, "numbering": nat.numbering}
         for nat in Nationality.objects.filter(numbering__isnull=False)]
-    
