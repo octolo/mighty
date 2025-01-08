b572e4894d47660fb97802e6799f7cb782703309
hicinformatic <hicinformatic@gmail.com>
2025-01-08 16:42:06 +0100

    fix: return default if strtoget null

--- functions/facilities.py
+++ functions/facilities.py
@@ -1,13 +1,14 @@
 def getattr_recursive(obj, strtoget, split=".", default=None):
-    keys = strtoget.split(split)
-    current = obj
-    try:
-        for key in keys:
-            if isinstance(current, dict):  # Si c'est un dictionnaire
-                current = current[key]
-            elif hasattr(current, key):    # Si c'est une classe avec l'attribut
-                current = getattr(current, key)
-        return current
-    except (KeyError, AttributeError, TypeError):
-        pass
+    if strtoget:
+        keys = strtoget.split(split)
+        current = obj
+        try:
+            for key in keys:
+                if isinstance(current, dict):  # Si c'est un dictionnaire
+                    current = current[key]
+                elif hasattr(current, key):    # Si c'est une classe avec l'attribut
+                    current = getattr(current, key)
+            return current
+        except (KeyError, AttributeError, TypeError):
+            pass
     return default
