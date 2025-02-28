67365e5ef1fb5e7a98255af4a3a2693b7f81ac44
acuD1 <arsciand@student.42.fr>
2025-02-28 09:48:57 +0100

    fix: JSONField from fields

--- fields.py
+++ fields.py
@@ -1,18 +1,19 @@
 from django.conf import settings
-from django.db import connection
 from django.db.models import TextField
 
 try:
     from django.db.models import JSONField
 except Exception:
-    from jsonfield import JSONField
+    from jsonfield import JSONField  # noqa
+
 
 def RichTextField(*args, **kwargs):
-    if "django_ckeditor_5" in settings.INSTALLED_APPS:
+    if 'django_ckeditor_5' in settings.INSTALLED_APPS:
         from django_ckeditor_5.fields import CKEditor5Field
+
         return CKEditor5Field(*args, **kwargs)
-    else:
-        return TextField(*args, **kwargs)
+    return TextField(*args, **kwargs)
+
 
 # Fields mighty
 uid = ('uid',)
@@ -43,14 +44,10 @@ base = (
     'search',
     'update_count',
     'cache',
-    'logs'
+    'logs',
 )
 
-news = (
-    'title',
-    'news',
-    'date_news'
-)
+news = ('title', 'news', 'date_news')
 
 _file = (
     'file',
@@ -78,68 +75,68 @@ export_url = ('export_url',)
 import_url = ('import_url',)
 
 # Admin URL
-admin_add_url = ('admin_add_url'),
-admin_list_url = ('admin_list_url'),
-admin_change_url = ('admin_change_url'),
-admin_delete_url = ('admin_delete_url'),
-admin_disable_url = ('admin_disable_url'),
-admin_enable_url = ('admin_enable_url'),
+admin_add_url = (('admin_add_url'),)
+admin_list_url = (('admin_list_url'),)
+admin_change_url = (('admin_change_url'),)
+admin_delete_url = (('admin_delete_url'),)
+admin_disable_url = (('admin_disable_url'),)
+admin_enable_url = (('admin_enable_url'),)
 
 channels = ('channel_name', 'channel_type', 'from_id', 'objs', 'history')
 
 backend = (
-    "service",
-    "content_type",
-    "backend",
-    "backend_list",
+    'service',
+    'content_type',
+    'backend',
+    'backend_list',
 )
 
 # Decorators
 
 immutable = (
-    "immutable",
-    "immutable_delete",
-    "immutable_fields",
+    'immutable',
+    'immutable_delete',
+    'immutable_fields',
 )
 
 reporting = (
-    "name",
-    "file_name",
-    "content_type",
-    "target",
-    "manager",
-    "config",
-    "filter_config",
-    "filter_related",
-    "filter_request",
-    "is_detail",
-    "can_excel",
-    "cfg_excel",
-    "can_csv",
-    "cfg_csv",
-    "can_pdf",
-    "cfg_pdf",
-    "html_pdf",
-    "email_html",
+    'name',
+    'file_name',
+    'content_type',
+    'target',
+    'manager',
+    'config',
+    'filter_config',
+    'filter_related',
+    'filter_request',
+    'is_detail',
+    'can_excel',
+    'cfg_excel',
+    'can_csv',
+    'cfg_csv',
+    'can_pdf',
+    'cfg_pdf',
+    'html_pdf',
+    'email_html',
 )
 
 registertask = (
-    "named_id",
-    "name",
-    "desc",
-    "register_type",
-    "content_type",
-    "is_enable_test",
-    "how_start_task",
+    'named_id',
+    'name',
+    'desc',
+    'register_type',
+    'content_type',
+    'is_enable_test',
+    'how_start_task',
 )
 
 registertasksubscription = (
-    "register",
-    "status",
-    "period",
-    "choiceday",
-    "last_date_task",
-    "object_id",
-    "content_type_subscriber",
-    "object_id_subscriber",
+    'register',
+    'status',
+    'period',
+    'choiceday',
+    'last_date_task',
+    'object_id',
+    'content_type_subscriber',
+    'object_id_subscriber',
 )
