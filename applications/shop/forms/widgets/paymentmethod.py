c9e86a5bc3caab306bc0c39d3a6a72c59f8e3bef
acuD1 <arsciand@student.42.fr>
2025-06-12 11:00:37 +0200

    refactor: remove old billing and shop apps

--- applications/shop/forms/widgets/paymentmethod.py
+++
@@ -1,6 +0,0 @@
-from django.forms.widgets import RadioSelect
-
-
-class PaymentmethodInput(RadioSelect):
-    input_type = 'paymentmethodinput'
-    template_name = 'shop/widgets/paymentmethod.html'
